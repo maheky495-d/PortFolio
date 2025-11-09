d6e22e5efbf013574bfe6d06438172fe18973390
maheky495-d <maheky234@gmail.com>
2025-11-09 17:50:11 +0530

    Lastt changesss

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@
 // This file is automatically detected by Vercel as a serverless function
 import app from "../server/index";
 
-// Export the Express app for Vercel
+// Export the Express app directly for Vercel
+// Vercel's @vercel/node automatically handles Express apps
 export default app;
 
